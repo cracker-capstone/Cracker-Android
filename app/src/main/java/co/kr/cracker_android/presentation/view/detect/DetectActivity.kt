b6cc1168e2d4de6c46d2fe74de2d9c4c86e73198
hansh0101 <hansh0101@naver.com>
2022-10-09 23:38:33 +0900

    [FEATURE] #5 - Android Location 권한을 사용해 위경도 추출 구현

--- app/src/main/java/co/kr/cracker_android/presentation/view/detect/DetectActivity.kt
+++ app/src/main/java/co/kr/cracker_android/presentation/view/detect/DetectActivity.kt
@@ -1,8 +1,9 @@
 package co.kr.cracker_android.presentation.view.detect
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.location.LocationManager
 import android.os.Bundle
 import androidx.camera.core.CameraSelector
 import androidx.camera.core.Preview
@@ -10,16 +11,20 @@ import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.core.content.ContextCompat
 import co.kr.cracker_android.R
 import co.kr.cracker_android.databinding.ActivityDetectBinding
+import co.kr.cracker_android.presentation.util.shortToast
 import co.kr.cracker_android.presentation.view.base.BaseActivity
 import timber.log.Timber
 
 class DetectActivity : BaseActivity<ActivityDetectBinding>() {
     override val layoutRes: Int
         get() = R.layout.activity_detect
+    private lateinit var locationManager: LocationManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
         startCamera()
+        initOnClickListener()
     }
 
     private fun startCamera() {
@@ -27,15 +32,12 @@ class DetectActivity : BaseActivity<ActivityDetectBinding>() {
         cameraProviderFuture.addListener({
             // Used to bind the lifecycle of cameras to the lifecycle owner
             val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
-
             // Preview
             val preview = Preview.Builder()
                 .build()
                 .also { it.setSurfaceProvider(binding.cameraPreview.surfaceProvider) }
-
             // Select back camera as a default
             val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
-
             runCatching {
                 // Unbind use cases before rebinding
                 cameraProvider.unbindAll()
@@ -43,8 +45,25 @@ class DetectActivity : BaseActivity<ActivityDetectBinding>() {
                 cameraProvider.bindToLifecycle(this, cameraSelector, preview)
             }.onFailure {
                 Timber.e("Use case binding failed $it")
+                shortToast("문제가 발생했습니다.")
+                finish()
             }
         }, ContextCompat.getMainExecutor(this))
+    }
+
+    private fun initOnClickListener() {
+        binding.btnStartDetect.setOnClickListener {
+            logCurrentLocation()
+        }
+    }
+
+    // Already checked in MainActivity
+    @SuppressLint("MissingPermission")
+    private fun logCurrentLocation() {
+        val currentLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER)
+            ?: locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER)
+        Timber.tag("location")
+            .i("위도: ${currentLocation?.latitude}, 경도: ${currentLocation?.longitude}")
     }
 
     companion object {
